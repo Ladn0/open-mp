2a5e5e556f66cf38a246647618954e9d8dfa4089
fd0000733 <artem.anufriyev@informatik.hs-fulda.de>
2024-11-22 16:07:07 +0100

    init

---
+++ matrix_vector_parallel.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+// #define SHAPE 10000
+#define RANGE 10
+
+int main(int argc)
+{
+
+    int SHAPE = 10;
+
+    for (int i = 0; i < 4; i++)
+    {
+        double wt1, wt2, wt3;
+
+        // srand(time(NULL));
+
+        int(*A)[SHAPE] = malloc((sizeof(int)) * SHAPE * SHAPE);
+        int(*x) = malloc((sizeof(int)) * SHAPE); // column vector x^T
+
+        int(*res) = malloc((sizeof(int)) * SHAPE);
+
+        wt1 = omp_get_wtime();
+
+#pragma omp parallel
+        {
+            unsigned int seed = time(NULL) ^ omp_get_thread_num();
+#pragma omp for
+            for (int i = 0; i < SHAPE; i++)
+            {
+                x[i] = rand_r(&seed) % RANGE;
+
+                for (int j = 0; j < SHAPE; j++)
+                {
+                    A[i][j] = rand_r(&seed) % RANGE;
+                }
+            }
+        }
+        wt2 = omp_get_wtime();
+
+#pragma omp parallel for
+        for (int i = 0; i < SHAPE; i++)
+        {
+            int temp = 0;
+            for (int j = 0; j < SHAPE; j++)
+            {
+                temp += A[i][j] * x[j];
+            }
+            res[i] = temp;
+        }
+        wt3 = omp_get_wtime();
+
+        // printf("Matrix A sample:\n");
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     for (int j = 0; j < 10; j++)
+        //     {
+        //         printf("%d ", A[i][j]);
+        //     }
+        //     printf("\n");
+        // }
+
+        // printf("\nVector x sample:\n");
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     printf("%d\n", x[i]);
+        // }
+
+        // printf("\nResult sample:\n");
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     printf("%d\n", res[i]);
+        // }
+
+        free(A);
+        free(x);
+        free(res);
+
+        // printf("\nMatrix & Vector initialization: %12.4g sec\n", wt2 - wt1);
+        // printf("\nMatrix & Vector multiplication: %12.4g sec\n", wt3 - wt2);
+        printf("\nTotal time: %12.4g sec\n", wt3 - wt1);
+        SHAPE *= 10;
+    }
+    return 0;
+}
---
+++ mergesort_serial.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+// #define INPUT_LENGTH 100000000
+void merge(int *arr, int left, int mid, int right)
+{
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int *leftArr = malloc((sizeof(int)) * n1);
+    int *rightArr = malloc((sizeof(int)) * n2);
+
+    // Copy data to temporary arrays
+    for (i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2)
+    {
+        if (leftArr[i] <= rightArr[j])
+        {
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of leftArr[], if any
+    while (i < n1)
+    {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of rightArr[], if any
+    while (j < n2)
+    {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+
+    free(leftArr);
+    free(rightArr);
+}
+
+void mergeSort(int *arr, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
+
+    int INPUT_LENGTH = 100;
+    for (int i = 0; i < 4; i++)
+    {
+
+        double wt1, wt2;
+
+        srand(time(NULL));
+
+        int *arr = malloc((sizeof(int)) * INPUT_LENGTH);
+
+        for (int i = 0; i < INPUT_LENGTH; i++)
+        {
+            arr[i] = rand() % INPUT_LENGTH;
+        }
+
+        wt1 = omp_get_wtime();
+
+        mergeSort(arr, 0, INPUT_LENGTH - 1);
+
+        wt2 = omp_get_wtime();
+
+        // for (int i = 0; i < INPUT_LENGTH; i++)
+        // {
+        //     printf("%d ", arr[i]);
+        // }
+
+        for (int i = 1; i < INPUT_LENGTH; i++)
+        {
+            if (arr[i] >= arr[i - 1])
+            {
+                continue;
+            }
+            else
+            {
+                printf("The sorting is wrong");
+                break;
+            }
+        }
+
+        printf("\nTotal time: %12.4g sec\n", wt2 - wt1);
+
+        free(arr);
+        INPUT_LENGTH = INPUT_LENGTH * 100;
+    }
+    return 0;
+}
